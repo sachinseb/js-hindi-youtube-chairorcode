8e626add69c91cfbdc9f8a4a3a39560ad6713ed1
Sachin <110269869+sachinseb@users.noreply.github.com>
2023-05-17 03:57:49 +0000

    new

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,7 @@
-// variables - to store information
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+let accountPassword  = "12345"
+accountCity = "Jaipur"
+
+accountId = 2
+console.log(accountId);
